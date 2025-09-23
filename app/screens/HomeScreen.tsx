f7bfe485d247fcd85ab2d24ee438da743791e2d9
pom <roy989898@gmail.com>
2025-09-23 19:09:44 +0100

    can use animation

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,20 +1,32 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Button, Text, View} from 'react-native';
 import type {NativeStackScreenProps} from '@react-navigation/native-stack';
 import type {RootStackParamList} from '../../App';
+import Animated, {useSharedValue, withSpring} from 'react-native-reanimated';
+import {SafeAreaView} from "react-native-safe-area-context";
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
 
 export default function HomeScreen({navigation}: Props): React.JSX.Element {
+
+    const width = useSharedValue(0);
+    const handlePress = () => {
+        width.value = withSpring(width.value + 10);
+    };
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-        >
-            <Text>Welcome to your Tarot App!</Text>
-        </View>
+        <SafeAreaView style={{flex: 1}}>
+            <View style={{flex: 1, alignItems: 'center'}}>
+                <Animated.View
+                    style={{
+                        width,
+                        height: 100,
+                        backgroundColor: 'violet',
+                    }}
+                />
+                <Button onPress={handlePress} title="Click me"/>
+            </View>
+        </SafeAreaView>
+
     );
+
 }
