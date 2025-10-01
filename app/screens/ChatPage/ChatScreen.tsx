d8eb7ba131d850f32c34fca873b2c7fab2447588
pom <roy989898@gmail.com>
2025-10-01 16:43:54 +0100

    save

--- app/screens/ChatPage/ChatScreen.tsx
+++ app/screens/ChatPage/ChatScreen.tsx
@@ -19,7 +19,7 @@ export default function ChatScreen() {
                 user: {
                     _id: 2,
                     name: 'React Native',
-                    avatar: 'https://placeimg.com/140/140/any',
+                    // avatar: 'https://placeimg.com/140/140/any',
                 },
             },
         ])
